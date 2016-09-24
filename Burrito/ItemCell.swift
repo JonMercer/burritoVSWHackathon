d4e3d53c0271ba09d2378a4551163716b43254c1
gordonseto <gordon.seto@gmail.com>
2016-09-24 15:43:42 -0700

    added redeemVC collection view

---
+++ Burrito/ItemCell.swift
@@ -0,0 +1,38 @@
+//
+//  ItemCell.swift
+//  Burrito
+//
+//  Created by Gordon Seto on 2016-09-24.
+//  Copyright © 2016 TeamAlpaka. All rights reserved.
+//
+
+import UIKit
+import expanding_collection
+
+class ItemCell: BasePageCollectionCell {
+
+    var cardView: CardView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+ 
+    class func instanceFromNib(frame: CGRect) -> ItemCell {
+        let view = UINib(nibName: "ItemCell", bundle: nil).instantiateWithOwner(nil, options: nil)[0] as! ItemCell
+        view.frame = frame
+        return view
+    }
+    
+    func configureCell(item: Item){
+        
+        cardView = CardView.instanceFromNib(CGRectMake(0, 0, 300, 300))
+        cardView.item = item
+        
+        self.frontContainerView.addSubview(cardView)
+    }
+    
+    func onCellTapped(){
+        cardView.changeModes()
+    }
+}
